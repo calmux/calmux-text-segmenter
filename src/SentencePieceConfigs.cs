362aaa1701fc4402dc643a47ccd6ef34521f0ab2
calmux <calmuxoto@proton.me>
2022-07-31 12:06:19 +0000

    upload to github

--- src/SentencePieceConfigs.cs
+++ src/SentencePieceConfigs.cs
@@ -6,4 +6,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Serializati
+using System.Xml.Serialization;
+
+namespace Microsoft.MT.Common.Tokenization
+{
+    // types for SentencePiece
+    public enum SentencePieceModelType
+    {
+        [XmlEnum
